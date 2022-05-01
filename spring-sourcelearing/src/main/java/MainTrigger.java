2a4c8435a6f08c5f73df7b313ea101a28a60aa48
黄嘉禾 <huangjiahe@369exhibition.com>
2022-05-01 17:39:11 +0800

    fork to spring main

--- spring-sourcelearing/src/main/java/MainTrigger.java
+++ spring-sourcelearing/src/main/java/MainTrigger.java
@@ -2,6 +2,7 @@ import com.sourcelearning.service.TestTriggerService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainTrigger {
+
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context =new AnnotationConfigApplicationContext(ScanConfig.class);
 		TestTriggerService triggerService = context.getBean(TestTriggerService.class);
