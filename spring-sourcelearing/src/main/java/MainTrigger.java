42e5497bf2fc0178272b87abc3962e4bb5363172
黄嘉禾 <huangjiahe@369exhibition.com>
2022-05-01 16:59:50 +0800

    源码配置build done

---
+++ spring-sourcelearing/src/main/java/MainTrigger.java
@@ -0,0 +1,11 @@
+import com.sourcelearning.service.TestTriggerService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainTrigger {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context =new AnnotationConfigApplicationContext(ScanConfig.class);
+		TestTriggerService triggerService = context.getBean(TestTriggerService.class);
+		triggerService.test();
+	}
+
+}
---
+++ spring-sourcelearing/src/main/java/com/sourcelearning/service/TestTriggerService.java
@@ -0,0 +1,12 @@
+package com.sourcelearning.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestTriggerService {
+	public void test(){
+		System.out.println("test!");
+	}
+
+
+}
